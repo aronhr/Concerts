f90a1a805c65bef9a2e92b8e8c08e9ae9e16df3d
Aron Hrafnsson <aronhr98@gmail.com>
2016-04-11 11:58:11 +0000

    Fixed if image fails to load

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 /**
  * Created by Aron on 11.4.2016.
  */
+var title = 'Tónleikar á Íslandi';
 
+$('.main').html('<h1 class="center">' + title + '');
 $.ajax({
     'url': 'https://apis.is/concerts',
     'type': 'GET',
@@ -9,18 +11,22 @@ $.ajax({
     'success': function(response) {
         for (var i = response.results.length - 1; i >= 0; i--) {
                 $('.main').append('' +
-                    '<div class="col s12 m4">' +
+                    '<div class="col s12 m3">' +
                         '<div class="card">' +
                             '<div class="card-image">' +
                                 '<img src="' + response.results[i].imageSource + '">' +
-                                '<span class="card-title">' + response.results[i].eventDateName + '</span>' +
                             '</div>' +
                             '<div class="card-content">' +
+                                '<span class="card-title">' + response.results[i].eventDateName + '</span>' +
                                 '<p>' + response.results[i].eventHallName + '</p>' +
                                 '<small>' + response.results[i].dateOfShow + '</small>' +
                             '</div>' +
                         '</div>' +
                     '</div>');
+            $("img").error(function () {
+                $(this).unbind("error").attr("src", "image/missing.jpg");
+            });
             }
         }
 });
+
