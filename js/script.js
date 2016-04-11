7f47999671eb9be15c3a432dd6d301b6510ca713
Aron Hrafnsson <aronhr98@gmail.com>
2016-04-11 08:42:07 +0000

    Concerts

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Aron on 11.4.2016.
+ */
+
+$.ajax({
+    'url': 'http://apis.is/concerts',
+    'type': 'GET',
+    'dataType': 'json',
+    'success': function(response) {
+        for (var i = response.results.length - 1; i >= 0; i--) {
+                $('.main').append('' +
+                    '<div class="col s12 m4">' +
+                        '<div class="card">' +
+                            '<div class="card-image">' +
+                                '<img src="' + response.results[i].imageSource + '">' +
+                                '<span class="card-title">' + response.results[i].eventDateName + '</span>' +
+                            '</div>' +
+                            '<div class="card-content">' +
+                                '<p>' + response.results[i].eventHallName + '</p>' +
+                                '<small>' + response.results[i].dateOfShow + '</small>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>');
+            }
+        }
+});
