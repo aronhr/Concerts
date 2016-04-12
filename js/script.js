20f3674f132a56a6991d9cab025c744a0c7eff55
Aron Hrafnsson <aronhr98@gmail.com>
2016-04-12 01:43:02 +0000

    Breytt yfir i bootstrap, sett preloader og lagað dagsetningu með Moment.js

--- js/script.js
+++ js/script.js
@@ -2,31 +2,49 @@
  * Created by Aron on 11.4.2016.
  */
 var title = 'Tónleikar á Íslandi';
+var errorcode = 'Því miður náðist ekki að sækja gögnin :/';
+/**
+ * Preloader svo að síðan sé ekki búin að loada á undan efninu
+ */
+jQuery(window).load(function() {
+    // will first fade out the loading animation
+    jQuery("#status").fadeOut();
+    // will fade out the whole DIV that covers the website.
+    jQuery("#preloader").delay(1000).fadeOut("slow");
+});
 
-$('.main').html('<h1 class="center">' + title + '');
+
+$('.main').html('<h1 class="text-center">' + title + '');
 $.ajax({
     'url': 'https://apis.is/concerts',
     'type': 'GET',
     'dataType': 'json',
     'success': function(response) {
-        for (var i = response.results.length - 1; i >= 0; i--) {
-                $('.main').append('' +
-                    '<div class="col s12 m3">' +
-                        '<div class="card">' +
-                            '<div class="card-image">' +
-                                '<img src="' + response.results[i].imageSource + '">' +
-                            '</div>' +
-                            '<div class="card-content">' +
-                                '<span class="card-title">' + response.results[i].eventDateName + '</span>' +
-                                '<p>' + response.results[i].eventHallName + '</p>' +
-                                '<small>' + response.results[i].dateOfShow + '</small>' +
-                            '</div>' +
+        for(var i = 0; i < response.results.length; i++){
+            var data = response.results;
+            var date = moment(data[i].dateOfShow).format('DD.MMMM YYYY, h:mm');
+            $('.main').append('' +
+                '<div class="col-sm-4 col-md-3">' +
+                    '<div class="thumbnail">' +
+                        '<img class="eventimage" src="' + data[i].imageSource + '">' +
+                        '<div class="caption">' +
+                            '<h4 class="ellipsis"><b>' + data[i].eventDateName + '</b></h4>' +
+                            '<p>' + data[i].eventHallName + '</p>' +
+                            '<small>' + date + '</small>' +
                         '</div>' +
-                    '</div>');
+                    '</div>' +
+                '</div>');
             $("img").error(function () {
                 $(this).unbind("error").attr("src", "image/missing.jpg");
             });
-            }
+
         }
+    }
+})
+.fail(function() {
+    $('.main').html('<h1 class="text-center">' + errorcode + '');
 });
+
+
+
 
